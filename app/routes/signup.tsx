ab2fe33e36705309b63cb0b04c0530f6748a95e7
Nicolas Derecho <nicolas@zype.com>
2023-04-21 17:43:21 -0300

    wip

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -20,6 +20,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
+  console.log(formData)
   
 }
 
